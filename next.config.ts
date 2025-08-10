095168af20d00f3d9ee4492e334ae6ae4bca5dbe
deadpandev <deadpangameryt@gmail.com>
2025-08-10 04:23:00 +0100

    Update Next.js configuration and development script
    - Removed allowed development origins from next.config.ts for a cleaner setup.
    - Updated the dev script in package.json to specify the host for local development.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["http://192.168.0.27", "http://192.168.0.3"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
