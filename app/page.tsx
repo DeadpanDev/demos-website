9f92126ecd902fc462a04c6f9ece8197b2bf779a
deadpandev <deadpangameryt@gmail.com>
2025-08-10 04:35:03 +0100

    Update home page layout and content
    - Enhanced the home page with a flexbox layout for better alignment.
    - Changed the heading to "Demos Website" and added descriptive text.
    - Included a "Coming soon" message to indicate future updates.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Demos Website</h1>
+      <p className="text-lg mt-1">
+        This is the demos website for the website project for clients to view.
+      </p>
+      <p className="text-3xl mt-4 font-bold uppercase">Coming soon</p>
     </div>
   );
 }
